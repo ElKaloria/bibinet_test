d0ef56fa5980530f3d635bab085262381d79fa86
ElKaloria <maks.dektyarev@yandex.ru>
2024-07-13 11:12:51 +0700

    create docker files and some requirements

--- bibinet_project/settings.py
+++ bibinet_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'part_app.apps.PartAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'bibinet_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BibinetDB',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres123',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
