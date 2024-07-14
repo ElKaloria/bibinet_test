4d6ffa3ab99b681f02e2c8ccbbeda797ac82ea7e
ElKaloria <maks.dektyarev@yandex.ru>
2024-07-14 12:04:16 +0700

    add pagination for views

--- part_app/views.py
+++ part_app/views.py
@@ -1,18 +1,27 @@
 
 from django.http import JsonResponse
 from django.core.serializers import serialize
+from django.core.paginator import Paginator
 from .models import *
 # Create your views here.
 
 
 def mark_view(request):
     qs = Mark.objects.all()
-    jsom_data = serialize('json', qs)
-    return JsonResponse(jsom_data, content_type='application/json', safe=False)
+    paginator = Paginator(qs, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    json_data = serialize('json', page_obj)
+    return JsonResponse({'data': json_data,
+                         'page': page_obj.number}, content_type='application/json', safe=False)
 
 
 def model_view(request):
     qs = Model.objects.all()
-    jsom_data = serialize('json', qs)
-    return JsonResponse(jsom_data, content_type='application/json', safe=False)
+    paginator = Paginator(qs, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    json_data = serialize('json', page_obj)
+    return JsonResponse({'data': json_data,
+                         'page': page_obj.number}, content_type='application/json', safe=False)
 
