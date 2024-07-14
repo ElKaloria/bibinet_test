0a4bf8ff2824a3d41b68c5027305e2073e68477b
ElKaloria <maks.dektyarev@yandex.ru>
2024-07-14 13:11:39 +0700

    create indexes for part and mark models

--- part_app/models.py
+++ part_app/models.py
@@ -9,6 +9,12 @@ class Mark(models.Model):
     producer_country_name = models.CharField(max_length=50)
     is_visible = models.BooleanField(default=True)
 
+    class Meta:
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['producer_country_name']),
+        ]
+
     def __str__(self):
         return self.name
 
@@ -31,6 +37,13 @@ class Part(models.Model):
     price = models.FloatField()
     json_data = models.JSONField(default=dict)
     is_visible = models.BooleanField(default=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['is_visible']),
+            models.Index(fields=['json_data']),
+        ]
 
     def __str__(self):
         return self.name
